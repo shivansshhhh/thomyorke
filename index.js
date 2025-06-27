4679f3ae5923ca105aa452bf81352721f25b28cc
shivansshhhh <167331111+shivansshhhh@users.noreply.github.com>
2025-06-28 00:52:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ import moment from "moment";
 import simpleGit from "simple-git";
 
 const path = "./data.json";
-const date = moment().format();
 
-const data = {
-  date: date,
+const makeCommits = (n) => {
+  if(n===0) return simpleGit().push();
+  const x = random.int(0, 54);
+  const y = random.int(0, 6);
+  const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+
+  const data = {
+    date: date,
+  };
+  console.log(date);
+  jsonfile.writeFile(path, data, () => {
+    simpleGit().add([path]).commit(date, { "--date": date },makeCommits.bind(this,--n));
+  });
 };
 
-jsonfile.writeFile(path, data);
-
-simpleGit().add([path]).commit(date,{'--date':date}).push();
-
-
+makeCommits(100);
